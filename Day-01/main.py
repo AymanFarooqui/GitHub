f5cfb1f4264531595f00996298a31c06de333730
Mohammed Ayman faroouqi <aimanfarooqui10@gmail.com>
2025-11-04 20:24:51 +0530

    Done with the Day-01

---
+++ Day-01/main.py
@@ -0,0 +1,7 @@
+# This is my first Git practice program
+
+def greet(name):
+    print(f"Hello, {name}! Welcome to my first Git project.")
+
+if __name__ == "__main__":
+    greet("Mohammed Ayman Farooqui")
